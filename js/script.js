a34e738879e88024a88c99a03548aa6fdb036190
lisahack1126 <lisa.hack1126@gmail.com>
2022-04-09 13:57:29 -0400

    Fixed API conditions response

--- js/script.js
+++ js/script.js
@@ -59,7 +59,7 @@ function displayWeatherCondition(response) {
     response.data.wind.speed
   );
   document.querySelector("#description").innerHTML =
-    response.data.weather[0].main;
+    response.data.weather[0].description;
   console.log(response.data);
   document.querySelector("#feels-like").innerHTML = Math.round(
     response.data.main.feels_like
