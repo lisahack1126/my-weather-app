54f709c190bcb0562dcc4e5c962742c092c16351
lisahack1126 <lisa.hack1126@gmail.com>
2022-04-10 10:15:53 -0400

    Added relevant icon from the API

--- js/script.js
+++ js/script.js
@@ -69,6 +69,15 @@ function displayWeatherCondition(response) {
   document.querySelector("#low").innerHTML = Math.round(
     response.data.main.temp_min
   );
+  document
+    .querySelector("#icon")
+    .setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+  document
+    .querySelector("#icon")
+    .setAttribute("alt", response.data.weather[0].description);
 }
 
 function search(city) {
